fa8307006234f98720236ac20b38318934006ef0
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2026-04-21 21:06:10 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,14 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "aws-iac-19159-tf"
+    key            = "terraform.tfstate-gha-tf"
+    region         = "us-east-1"
+    encrypt        = true
+    use_lockfile   = true
+  }
+}
+
